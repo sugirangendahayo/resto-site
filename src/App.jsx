ce3f55875cf49f79c8dec9185bfd54519fa697be
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-05-11 17:14:02 +0200

    Update App.jsx to include new components and remove theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import Navbar from "./components/common/Navbar";
+import Hero from "./components/Hero";
+import FeaturedItems from "./components/FeaturedItems";
+import HowItWorks from "./components/HowItWorks";
+import Testimonials from "./components/Testimonials";
 
-function App() {
-
-
-  return <></>;
-}
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <Navbar />
+      <main>
+        <Hero />
+        <FeaturedItems />
+        <HowItWorks />
+        <Testimonials />
+      </main>
+    </div>
+  );
+};
 
 export default App;
