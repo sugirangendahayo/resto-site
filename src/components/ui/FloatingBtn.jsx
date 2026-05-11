39b8fedaf24bae59543723a658ae3f26a214585d
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-05-11 17:17:01 +0200

    Update FloatingBtn component - remove theme functionality

---
+++ src/components/ui/FloatingBtn.jsx
@@ -0,0 +1,23 @@
+const FloatingBtn = () => {
+  return (
+    <>
+      <button className=" bg-orange-500  rounded-full absolute bottom-[10%] right-[5%] p-2 text-white cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+          />
+        </svg>
+      </button>
+    </>
+  );
+};
+export default FloatingBtn;
